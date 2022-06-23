2e4ade9a111fdc2aad1d6ecd1e557afd2b82b880
zenf <78023279+zencodee@users.noreply.github.com>
2022-06-23 10:20:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App () {
+  return (
+    <div className=' h-screen text-pink-300 text-xl bg-black grid place-items-center'>
+      react-vite-tailwind starter
+    </div>
+  )
+}
+
+export default App
